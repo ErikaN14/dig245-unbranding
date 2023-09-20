5031684ca6e56bf7aecd063f64540f7a489f292e
Erika Navarro <erika072002@gmail.com>
2023-09-20 00:34:18 -0400

    Implemented Hover (09/20/2023)
    Got hover function to work and image goes from original logo to unbranded logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,26 @@
-
 /* javascript */
+
+// main.js
+/** 
+// 1 - This is just a test
+console.log("Hello from main.js");
+
+// 2 - Store a reference to the target element
+let targetElement = document.querySelector(".cssBackgrounImg:hover");
+
+// 3 - Log the reference to confirm it works
+console.log(targetElement);
+
+// 4 - Add a mouseover event listener
+targetElement.addEventListener("mouseover", mouseoverHandler);
+
+// 5 - Create a handler function to run code when the event happens
+function mouseoverHandler() {
+   //console.log(this.innerText);
+    this.style = "cursor: wait"; // 7 - Update the cursor style
+}
+
+// 6 - An event listener and handler in the same statement
+targetElement.addEventListener("mouseout", function () {
+    this.style = "cursor: pointer";*/
+
